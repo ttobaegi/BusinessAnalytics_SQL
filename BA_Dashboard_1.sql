c39ca27695e77aff4653c65b9cef5e868b6d35cc
ttobaegi <ttobaegi0308@gmail.com>
2021-03-10 08:26:25 +0900

    0310

--- BA_Dashboard_1.sql
+++ BA_Dashboard_1.sql
@@ -1,1 +1,17 @@
 ## Dashboarding with Tableau
+SELECT * FROM website_pageviews LIMIT 10
+
+
+SELECT MAX(created_at)-MIN(created_at) AS Duration
+FROM website_pageviews
+GROUP BY website_session_id
+
+
+SELECT * FROM ( SELECT website_session_id,
+		COUNT(DISTINCT website_pageview_id)
+FROM website_pageviews
+GROUP BY website_session_id
+) A
+LEFT JOIN website_sessions ON A.website_session_id = website_sessions.website_session_id
+
+
