6bd84267b21b4ed1c102953f25367de8bfc3f618
ttobaegi <ttobaegi0308@gmail.com>
2021-03-16 00:38:38 +0900

    0316

--- Advanced_mysql_1.sql
+++ Advanced_mysql_1.sql
@@ -1,3 +1,6 @@
+## Advanced_mysql_1
+# Test data 생성
+
 CREATE SCHEMA testdb;
 use testdb;
 
@@ -27,15 +30,65 @@ select d1.dname as '상위부서', d2.*
   
 select * from Emp;
 
-select CEIL(rand() * 7);
-select char_length('한들abc');
+select rand()						# 0-1 난수 생성
+select rand()*10					# 1-10 난수 생성 
+select CEIL(rand() * 7);			# 소숫점 올림 CEIL 
+select length('한들abc');			# 바이트 수 LENGTH
+select char_length('한들abc');		# 글자 수 CHAR_LENGTH
 
-select f_rand1('1234567');
+select CEIL(rand()*10);
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+/** Stored Function 생성 
+CREATE FUNCTION `f_rand1` (_str varchar(255))		
+RETURNS varchar(31)						
+BEGIN
+	DECLARE v_ret varchar(31);					
+    DECLARE v_len tinyint;
+    
+    SET v_len = char_length(_str);
+    SET v_ret = substring(_str, CEIL(rand() * v_len), 1);
+    
+RETURN v_ret;
+END
+**/
+
+select f_rand1('1234567');		# 부서 1-7
+
+/** Stored Function 생성 
+CREATE FUNCTION `f_randname`() 
+RETURNS varchar(31) 
+BEGIN
+	DECLARE v_ret varchar(31);
+    DECLARE v_lasts varchar(244) default '김이박조전최천방지마유배원';
+    DECLARE v_firsts varchar(244) default '순신세종성호지혜가온세호은국가나다라마바사아자차파태결찬희';
+    
+    set v_ret = concat(f_rand1(v_lasts), f_rand1(v_firsts), f_rand1(v_firsts) );
+
+RETURN v_ret;
+END
+**/
+
 select f_randname();
+
 desc Emp;
 insert into Emp(ename, dept, salary) values (f_randname(), f_rand1('34567'), f_rand1('123456789') * 100);
+/** Stored Procedure 반복작업 수행
+CREATE PROCEDURE `sp_test_emp`(_cnt int)
+BEGIN
+	declare v_idx int default 0;		# 변수 선언 
+	
+    while v_idx < cnt
+    do 
+		insert into Emp(ename, dept, salary) values (f_randname(), f_rand1('34567'), f_rand1('123456789') * 100);
+		set v_idx = v_idx + 1 ;
+    end while;
+END
 
+**/
 call sp_test_emp(250);
+select * from emp;			
 
 select dept, count(*) from Emp group by dept;
 
